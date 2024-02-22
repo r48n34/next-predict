738d8a5710e67087ddd1db0f2cad66d4479ae07a
reemo <r48n34@gmail.com>
2024-02-22 18:44:48 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,13 +15,14 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
 
         <meta name="description" content="Is a website" />
+        <meta name="google-site-verification" content="MSmy-J4XDNXLBlFKqvooWG7RzHPvXmjhSo1BWtgF40E" />
 
         <meta property="og:title" content="Is a website" />
         <meta property="og:description" content="Is a website" />
         <meta property="og:locale" content="en_GB" />
         <meta property="og:type" content="website" />
 
-        <meta property="og:url" content="https://github.com/"></meta>
+        <meta property="og:url" content="https://github.com/" />
         <meta name="theme-color" content="#1a1b1e" />
       </Head>
 
